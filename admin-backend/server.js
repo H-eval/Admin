5b62d8218203043eaef3d4cd274fc97e48a76c75
Aaral-Agrawal <aaralagrawal7@gmail.com>
2026-03-31 02:52:03 +0530

    fix nodemailer config

--- admin-backend/server.js
+++ admin-backend/server.js
@@ -35,9 +35,7 @@ mongoose
 
 // Email setup
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: process.env.EMAIL_PORT == 465,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
